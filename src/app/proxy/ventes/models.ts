a1d251c7cd435faa712d6f05371ee997b568f6f8
zoubir00 <103057961+zoubir00@users.noreply.github.com>
2023-09-24 15:56:31 +0100

    Fetch the ventes released by the clients

---
+++ src/app/proxy/ventes/models.ts
@@ -0,0 +1,4 @@
+
+// export interface ClientDto extends EntityDto<number>{
+
+// }
--- src/app/vente/vente.component.ts
+++ src/app/vente/vente.component.ts
@@ -1,13 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { VenteService } from '@proxy/controllers';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms'; // for form
+import { ConfirmationService, Confirmation } from '@abp/ng.theme.shared';
 
 @Component({
   selector: 'app-vente',
   templateUrl: './vente.component.html',
-  styleUrls: ['./vente.component.scss']
+  styleUrls: ['./vente.component.scss'],
+  providers: [ListService], 
 })
-export class VenteComponent {
+export class VenteComponent implements OnInit {
+  ventes:any;
+constructor(
+  public readonly list:ListService,
+  private venteService:VenteService,
+  private fb:FormBuilder,
+  private confirmation:ConfirmationService
+  ) {}
+
+  ngOnInit(): void {
+    this.venteService.getVentes().subscribe((response) => {
+      // Handle the response from the service, e.g., assign it to a component property
+       this.ventes = response; // Adjust this based on your service response structure
+      console.log('Ventes:', this.ventes);
+    });
+  }
 
 }
